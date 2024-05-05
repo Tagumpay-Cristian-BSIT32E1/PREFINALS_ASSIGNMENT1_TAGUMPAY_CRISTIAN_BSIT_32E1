8a4da9a63735ef704f12abbd01a244fb88f55f21
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-05-06 00:38:05 +0800

    Added codes to AuthController

--- PREFINALS_AUTH_TAGUMPAY_CRISTIAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_TAGUMPAY_CRISTIAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8faad9511d5fee8eb322631df93a8cd8bf83501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93496446a16e5802738a9406c722173d680fd12d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
