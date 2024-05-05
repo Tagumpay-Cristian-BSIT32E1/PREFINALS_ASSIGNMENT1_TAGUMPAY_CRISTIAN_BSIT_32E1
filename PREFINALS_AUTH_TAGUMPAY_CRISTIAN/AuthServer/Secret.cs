7b23651ca19304a68de47cd7bd9d08b222e64ea9
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-05-06 00:26:41 +0800

    Added Password in Secret

---
+++ PREFINALS_AUTH_TAGUMPAY_CRISTIAN/AuthServer/Secret.cs
@@ -0,0 +1,4 @@
+﻿public static class Secret
+{
+    public const string Key = "Cristian_Jay_Tagumpay_The_Passwords";
+}
--- PREFINALS_AUTH_TAGUMPAY_CRISTIAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_TAGUMPAY_CRISTIAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9324e4e803df0536b179d718fa9326f4b5ad0081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36365628e1268a87dad0b74189cd934e6418b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
