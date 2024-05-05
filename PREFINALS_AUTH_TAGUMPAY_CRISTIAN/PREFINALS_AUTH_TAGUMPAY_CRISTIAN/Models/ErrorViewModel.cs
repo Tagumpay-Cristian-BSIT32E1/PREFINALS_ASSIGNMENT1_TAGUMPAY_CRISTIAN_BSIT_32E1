041f323c6680769f346141aaf2765661b210d06d
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-05-06 00:12:03 +0800

    Setup

---
+++ PREFINALS_AUTH_TAGUMPAY_CRISTIAN/PREFINALS_AUTH_TAGUMPAY_CRISTIAN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_TAGUMPAY_CRISTIAN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
