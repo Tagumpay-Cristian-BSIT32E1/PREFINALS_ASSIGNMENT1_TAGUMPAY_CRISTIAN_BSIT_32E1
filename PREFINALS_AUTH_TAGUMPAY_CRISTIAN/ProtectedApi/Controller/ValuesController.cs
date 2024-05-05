4f54a7f852f3aa4bebe87dff903aca4036cf5e6f
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-05-06 01:10:45 +0800

    Fixed errors

--- PREFINALS_AUTH_TAGUMPAY_CRISTIAN/ProtectedApi/Controller/ValuesController.cs
+++ PREFINALS_AUTH_TAGUMPAY_CRISTIAN/ProtectedApi/Controller/ValuesController.cs
@@ -11,15 +11,15 @@ namespace ProtectedApi
         private readonly Random _random = new Random();
         private readonly string[] _thingsAboutOwner = new[]
         {
-            "I sleep a lot."
-            "I like watching horror movies alone at night."
-            "I'm more than 20 years old."
-            "I have dog named Anti Leng."
+            "I sleep a lot.",
+            "I like watching horror movies alone at night.",
+            "I'm more than 20 years old.",
+            "I have dog named Anti Leng.",
             "Currently taking BSIT.",
-            "I like Fast and Furious Movies."
-            "Watch Suits on Netflix. It's fun."
-            "I don't drink coffee, it's not for kids."
-            "I don't drink soda."
+            "I like Fast and Furious Movies.",
+            "Watch Suits on Netflix. It's fun.",
+            "I don't drink coffee, it's not for kids.",
+            "I don't drink soda.",
             "This is the 10th."
         };
 
